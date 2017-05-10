40e9261282a02782c89d7a029dfdf7807ff1ee25
Ericwyn <Ericwyn.chen@gmail.com>
2017-05-10 19:25:13 +0800

    修改了文件工具类，增加了计算文件大小的方法

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -3,6 +3,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.text.DecimalFormat;
+
+import sun.rmi.runtime.Log;
 
 /**
  * 封装文件的增删改，和文件、文件夹的复制，粘贴
@@ -11,6 +14,11 @@ import java.nio.channels.FileChannel;
  * Created by Ericwyn on 17-5-2.
  */
 public class FileUtils {
+
+    private static final int SIZETYPE_B = 1;//获取文件大小单位为B的double值
+    private static final int SIZETYPE_KB = 2;//获取文件大小单位为KB的double值
+    private static final int SIZETYPE_MB = 3;//获取文件大小单位为MB的double值
+    private static final int SIZETYPE_GB = 4;//获取文件大小单位为GB的double值
 
     /**
      * 创建文件
@@ -194,6 +202,140 @@ public class FileUtils {
         }
         System.out.println("移动文件夹"+dirFromPath+"失败");
         return false;
+    }
+
+    /**
+     * 获取文件指定文件的指定单位的大小
+     * @param filePath 文件路径
+     * @param sizeType 获取大小的类型1为B、2为KB、3为MB、4为GB
+     * @return double值的大小
+     */
+    public static double getFileOrFilesSize(String filePath,int sizeType){
+        File file=new File(filePath);
+        long blockSize=0;
+        try {
+            if(file.isDirectory()){
+                blockSize = getFileSizes(file);
+            }else{
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return FormetFileSize(blockSize, sizeType);
+    }
+
+    /**
+     * 调用此方法自动计算指定文件或指定文件夹的大小
+     * @param filePath 文件路径
+     * @return 计算好的带B、KB、MB、GB的字符串
+     */
+    public static String getAutoFileOrFilesSize(String filePath){
+        File file=new File(filePath);
+        long blockSize=0;
+        try {
+            if(file.isDirectory()){
+                blockSize = getFileSizes(file);
+            }else{
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return FormetFileSize(blockSize);
+    }
+
+    /**
+     * 获取指定文件的大小
+     * @param file  文件
+     * @return  返回大小
+     * @throws Exception    抛出异常
+     */
+    private static long getFileSize(File file) throws Exception
+    {
+        long size = 0;
+        if (file.exists()){
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            size = fis.available();
+        }
+        else{
+            file.createNewFile();
+        }
+        return size;
+    }
+
+    /**
+     * 获取指定文件夹的大小
+     * @param f
+     * @return
+     * @throws Exception
+     */
+    private static long getFileSizes(File f) throws Exception {
+        long size = 0;
+        File flist[] = f.listFiles();
+        for (int i = 0; i < flist.length; i++){
+            if (flist[i].isDirectory()){
+                size = size + getFileSizes(flist[i]);
+            }
+            else{
+                size =size + getFileSize(flist[i]);
+            }
+        }
+        return size;
+    }
+    /**
+     * 转换文件大小
+     * @param fileS
+     * @return
+     */
+    private static String FormetFileSize(long fileS) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        String wrongSize="0B";
+        if(fileS==0){
+            return wrongSize;
+        }
+        if (fileS < 1024){
+            fileSizeString = df.format((double) fileS) + "B";
+        }
+        else if (fileS < 1048576){
+            fileSizeString = df.format((double) fileS / 1024) + "KB";
+        }
+        else if (fileS < 1073741824){
+            fileSizeString = df.format((double) fileS / 1048576) + "MB";
+        }
+        else{
+            fileSizeString = df.format((double) fileS / 1073741824) + "GB";
+        }
+        return fileSizeString;
+    }
+    /**
+     * 转换文件大小,指定转换的类型
+     * @param fileS
+     * @param sizeType
+     * @return
+     */
+    private static double FormetFileSize(long fileS,int sizeType) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double fileSizeLong = 0;
+        switch (sizeType) {
+            case SIZETYPE_B:
+                fileSizeLong=Double.valueOf(df.format((double) fileS));
+                break;
+            case SIZETYPE_KB:
+                fileSizeLong=Double.valueOf(df.format((double) fileS / 1024));
+                break;
+            case SIZETYPE_MB:
+                fileSizeLong=Double.valueOf(df.format((double) fileS / 1048576));
+                break;
+            case SIZETYPE_GB:
+                fileSizeLong=Double.valueOf(df.format((double) fileS / 1073741824));
+                break;
+            default:
+                break;
+        }
+        return fileSizeLong;
     }
 
 }
